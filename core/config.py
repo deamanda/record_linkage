a7f0488b0ba130d755576478ad3fc106c5db2e71
Rashid Ayoupov <valentaine1234@gmail.com>
2023-11-25 17:25:58 +0300

    настройки автомиграций

--- core/config.py
+++ core/config.py
@@ -3,7 +3,9 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     api_v1_prefix: str = "/api/v1"
-    db_url: str = "postgresql+asyncpg://rashid:12061998@localhost:5432/rashid"
+    db_url: str = (
+        "postgresql+asyncpg://{name}:{password}@localhost:5432/{name}"
+    )
     db_echo: bool = False
 
 
