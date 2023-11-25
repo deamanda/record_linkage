c2da22c05f873dce35fa5ef7f378f44e9e68c192
Rashid Ayoupov <valentaine1234@gmail.com>
2023-11-25 15:27:06 +0300

    Руты в мейн

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import uvicorn
 from fastapi import FastAPI
+from routing.v1.products import router as router_v1
 
 
 app = FastAPI()
-
+app.include_router(router_v1, prefix="/v1")
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
