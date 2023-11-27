587059169141498bfb05a4015903271e174f16b3
Rashid Ayoupov <valentaine1234@gmail.com>
2023-11-27 18:26:37 +0300

    Получение товаров и товаров по id

--- api/v1/products/routing.py
+++ api/v1/products/routing.py
@@ -1,8 +1,9 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import APIRouter, Depends
 
-from api.v1.products.repositories import get_products
-from api.v1.products.schemas import ProductResponse
+from api.v1.products.depends import product_by_id
+from api.v1.products.repositories import get_products, get_product
+from api.v1.products.schemas import ProductResponse, Product
 from fastapi import File, UploadFile
 from core.db_helper import db_helper
 from fastapi import Query
@@ -19,6 +20,15 @@ async def imports_product_csv(
 ):
     await imports_product(file, session)
     return {"message": "CSV data imported successfully"}
+
+
+@router.get(
+    "/{product_id}/",
+    response_model=Product,
+    summary="Получить товар заказчика",
+)
+async def get_product(product: Product = Depends(product_by_id)):
+    return product
 
 
 @router.get(
