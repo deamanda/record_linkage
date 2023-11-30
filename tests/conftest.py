410f19e06455990d98f8fe517b5282ecc5e95ec4
deamanda <deamanda@ya.ru>
2023-11-30 09:11:49 +0500

    connection to test db

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,26 @@ import httpx
 import pytest
 import pytest_asyncio
 from asgi_lifespan import LifespanManager
+import core.config as conf
+from core.config import Settings
+
+TEST_DB = "postgresql+asyncpg://postgres:postgres@localhost:5432/tests"
+
+conf.settings = Settings(db_url=TEST_DB)
+
 from main import app
+
+@pytest.fixture(autouse=True, scope="session")
+def run_migrations() -> None:
+    from alembic.config import Config
+    from alembic import command
+
+    alembic_text_cfg = Config()
+    alembic_text_cfg.set_main_option('script_location', 'alembic')
+    alembic_text_cfg.set_main_option('sqlalchemy.url', TEST_DB)
+    command.upgrade(alembic_text_cfg, 'head')
+    yield
+    command.downgrade(alembic_text_cfg, 'base')
 
 
 @pytest.fixture(scope="session")
@@ -15,7 +34,7 @@ def event_loop():
 
 @pytest_asyncio.fixture
 async def test_client():
-    async with LifespanManager(app):
+    async with LifespanManager(app): 
         async with httpx.AsyncClient(
             app=app, base_url='http://test/api/v1/') as test_client:
             yield test_client
--- tests/test.py
+++ tests/test.py
@@ -1,16 +1,26 @@
 import pytest
 from fastapi import status
+from pathlib import Path
+
 
 pytestmark = pytest.mark.asyncio
 
 
-async def test_get_products_ok(test_client):
-    response = await test_client.get('products/')
+async def test_post_products(test_client):
+    path = Path(Path.cwd(), 'tests/test_files', 'products.csv')
+    with open(path, 'rb') as f:
+        file = {'file': f}
+        response = await test_client.post('products/import-csv/', files=file)
+        assert response.status_code == status.HTTP_200_OK
+
+
+async def test_get_products(test_client):
+    response = await test_client.get('products')
 
     assert response.status_code == status.HTTP_200_OK
 
 
-async def test_get_dealers_price_ok(test_client):
+async def test_get_dealers_price(test_client):
     response = await test_client.get('dealers/price/')
 
     assert response.status_code == status.HTTP_200_OK
