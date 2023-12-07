ab610bac929551635d980bde23b29ccf47ce715e
deamanda <deamanda@ya.ru>
2023-12-08 04:59:55 +0500

    exception in loop

--- tests/conftest.py
+++ tests/conftest.py
@@ -30,7 +30,10 @@ def run_migrations() -> None:
 
 @pytest.fixture(scope='session')
 def event_loop():
-    loop = asyncio.get_event_loop_policy().new_event_loop()
+    try:
+        loop = asyncio.get_running_loop()
+    except RuntimeError:
+        loop = asyncio.new_event_loop()
     yield loop
     loop.close()
 
