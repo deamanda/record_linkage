7abd922c38bc8038bc4ae4d97e8ea6891221fc24
deamanda <deamanda@ya.ru>
2023-12-08 04:45:02 +0500

    policy?

--- tests/conftest.py
+++ tests/conftest.py
@@ -30,7 +30,7 @@ def run_migrations() -> None:
 
 @pytest.fixture(scope='session')
 def event_loop():
-    loop = asyncio.new_event_loop()
+    loop = asyncio.get_event_loop_policy().new_event_loop()
     yield loop
     loop.close()
 
